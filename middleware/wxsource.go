88d06a8a9509fd5019d5b6b94ac43523ee4922ee
riou00 <riou00@qq.com>
2026-07-16 08:04:19 +0800

    Update wxsource.go

--- middleware/wxsource.go
+++ middleware/wxsource.go
@@ -11,11 +11,11 @@ import (
 
 // WXSourceMiddleWare 中间件 判断是否来源于微信
 func WXSourceMiddleWare(c *gin.Context) {
-	if _, ok := c.Request.Header[http.CanonicalHeaderKey("x-wx-source")]; ok {
+	//if _, ok := c.Request.Header[http.CanonicalHeaderKey("x-wx-source")]; ok {
 		fmt.Println("[WXSourceMiddleWare]from wx")
 		c.Next()
-	} else {
+	//} else {
 		c.Abort()
-		c.JSON(http.StatusUnauthorized, errno.ErrNotAuthorized)
-	}
+	//	c.JSON(http.StatusUnauthorized, errno.ErrNotAuthorized)
+//	}
 }
